dff08ec643cdbcc24a2a790a43f0b08406f9f17c
Valentin Niess <niess@in2p3.fr>
2023-05-30 11:14:40 +0200

    Uproot serialisation for CaloRado generator

--- examples/calorado/generator.py
+++ examples/calorado/generator.py
@@ -1,28 +1,29 @@
 #! /usr/bin/env python3
-import matplotlib.pyplot as plot
 from mulder import Direction, Fluxmeter, Grid, PixelGrid
 import numpy
+import uproot 
 
-# import uproot 
-
-
+# Create the mulder fluxmeter object using the DEMs as arguments
 fluxmeter = Fluxmeter(
     Rock  = "data/mns_roche.png",
     Water = "data/mns_eau.png"
 )
 
+# Specify the calculation mode
 fluxmeter.mode = "continuous"
-
 
 def generate_events(latitude, longitude, depth, radius, n):
     """Generate events for given coordinates."""
 
     # Get map coordinates of platform and corresponding water level.
-    layer = fluxmeter.geometry.layers[1]
-    platform = layer.project(latitude, longitude) # To Lambert
-    water_level = layer.height(platform)
+    layer = fluxmeter.geometry.layers[1] # Water layer
+    platform = layer.project(latitude, longitude) # Coordinates of the platform (To Lambert)
+    water_level = layer.height(platform) # Altitude of the platform
 
-    costheta = fluxmeter.prng(n)
+    # Generate random point coordinates on the source sphere
+    # theta is polar angle counted from vertical axis (z)
+    # phi is orientation angle
+    costheta = fluxmeter.prng(n) 
     phi = fluxmeter.prng(n) * 2 * numpy.pi
     sintheta = numpy.sqrt(1 - costheta**2)
     normal_x = sintheta * numpy.cos(phi)
@@ -32,6 +33,7 @@ def generate_events(latitude, longitude, depth, radius, n):
     y = radius * normal_y 
     z = radius * normal_z 
 
+    # Express the point coordinates in the reference frame of mulder
     position = layer.position(platform.x + x, platform.y + y)
     position.height = water_level - depth + z
     weight = 2 * numpy.pi * radius**2 # 1 / PDF
@@ -39,6 +41,7 @@ def generate_events(latitude, longitude, depth, radius, n):
     # Generate direction.
     costheta = fluxmeter.prng(n)
     phi = fluxmeter.prng(n) * 2 * numpy.pi
+    sintheta = numpy.sqrt(1 - costheta**2)
     direction_x = sintheta * numpy.cos(phi)
     direction_y = sintheta * numpy.sin(phi)
     direction_z = costheta
@@ -67,22 +70,45 @@ def generate_events(latitude, longitude, depth, radius, n):
         normal_y * direction_y + normal_z * direction_z) # Hz
     rate /= n
 
-    # Print events.
+    # Print events. First, let us select ingoing events (i.e. with a positive
+    # rate).
     mm = 1E+03
-    MeV = 1E-03
-    for i, ri in enumerate(rate):
-        if ri < 0: continue
-        print(
-            f"mu- "
-            f"{x[i] * mm:8.0f} "
-            f"{y[i] * mm:8.0f} "
-            f"{z[i] * mm:8.0f} "
-            f"{direction_x[i]:12.5E} "
-            f"{direction_y[i]:12.5E} "
-            f"{direction_z[i]:12.5E} "
-            f"{energy[i] * MeV:12.5E} "
-            f"{ri:12.5E} "
-        )
+    cm = 1E+02
+    MeV = 1E+03
+    good = numpy.where(rate >= 0)
+    xW = x[good]*mm
+    yW = y[good]*mm
+    zW = z[good]*mm
+    dxW = -direction_x[good]
+    dyW = -direction_y[good]
+    dzW = -direction_z[good]
+    eW = energy[good]*MeV
+    pid = len(eW) * [13]
+    weight = numpy.ones(xW.shape)
+
+    # Then, data are converted to a numpy structured array, for serialisation
+    # with uproot. Note that GATE expects float32 data, not float64.
+    dtype = [
+        ("PDGCode", "i4"),
+        ("X", "f4"),
+        ("Y", "f4"),
+        ("Z", "f4"),
+        ("dX", "f4"),
+        ("dY", "f4"),
+        ("dZ", "f4"),
+        ("Ekine", "f4"),
+        ("Weight", "f4"),
+    ]
+
+    array = numpy.array(
+        list(zip(pid, xW, yW, zW, dxW, dyW, dzW, eW, weight)),
+        dtype = dtype
+    )
+
+    # Finally, we Write data to a ROOT file
+    with uproot.recreate("events.root") as file:
+        file["PhaseSpace"] = array
+        file["PhaseSpace"].show()
 
 
 if __name__ == "__main__":
@@ -92,6 +118,6 @@ if __name__ == "__main__":
         latitude = latitude,
         longitude = longitude,
         depth = 10,
-        radius = 0.2,
+        radius = 0.1,
         n = 100
     )
